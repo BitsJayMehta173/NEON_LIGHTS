83654ff0894d2d0281f01c07f1d1c98d56813c90
Jay Mehta <jaymehta20020605@gmail.com>
2021-07-07 08:46:55 +0545

    Made a Front View and Added a song to be played

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function loaded() {
         
      }, 3000);   //change the time to 3000ms after test
 }
+
+const btnplay = document.getElementById('btnplay');
+const mysong1 = document.getElementById('song1');
+function songplay() {
+    
+    if(mysong1.paused)
+    {
+        mysong1.play();
+        btnplay.innerHTML='Pause';
+    }
+    else{
+        mysong1.pause();
+        btnplay.innerHTML = 'Play';
+    }
+};
